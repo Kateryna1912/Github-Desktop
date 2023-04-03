c8c6d258954a3e769c5f7403f01f34c2175cfe34
Kateryna1912 <catherinebestanchuk@gmail.com>
2023-04-03 20:31:13 +0200

    add

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ nameInput.required = "required";
 let userNameLabel = document.createElement("label");
 userNameDiv.appendChild(userNameLabel);
 userNameLabel.innerHTML = "Username";
+
+let userPassDiv = document.createElement("div");
+createForm.appendChild(userPassDiv);
+userPassDiv.className = "userBox";
+
+let passInput = document.createElement("input");
+userPassDiv.appendChild(passInput);
+passInput.type = "password";
+passInput.required = "required";
+
+let userPassLabel = document.createElement("label");
+userPassDiv.appendChild(userPassLabel);
+userPassLabel.innerHTML = "Password";
+
+let subButtom = document.createElement("button");
+createForm.appendChild(subButtom);
+subButtom.innerHTML = "submit";
+subButtom.style.cursor = "pointer";
+for (let i = 0; i < 4; i++) {
+  let createSpan = document.createElement("span");
+  subButtom.appendChild(createSpan);
+}
